6bd0c221270c3ffbecd9b226e967b95c994d252b
최정필 <wjdvlf1407@gmail.com>
2023-03-31 19:46:33 +0900

    dd

--- src/week2/Ex2446_별찍기9.java
+++ src/week2/Ex2446_별찍기9.java
@@ -5,34 +5,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Ex2439 {
+public class Ex2446_별찍기9 {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		int n = Integer.parseInt(st.nextToken());
-		int t = n;
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j < t; j++) {
-
-				System.out.print(" ");
-
-			}
-
-			for (int k = 1; k < i + 1; k++) {
-				System.out.print("*");
-
-			}
-
-			t--;
-
-			System.out.println("");
-
+		
+		for (int i = 0; i < n*2; i++) {
+			
 		}
+		
 
+		
 		br.close();
 	}
 

2e21eaa07c5b8afb1030d361d6433a75d8ff8bf7
최정필 <wjdvlf1407@gmail.com>
2023-04-11 18:35:45 +0900

    백준 2446번 별 찍기 - 9

--- src/week2/Ex2446_별찍기9.java
+++ src/week2/Ex2446_별찍기9.java
@@ -3,6 +3,7 @@ package week2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Ex2446_별찍기9 {
@@ -11,18 +12,33 @@ public class Ex2446_별찍기9 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		
-		
-		for (int i = 0; i < n*2; i++) { 
-		
-			
-			
-		}
-		
 
-		
+		int n = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 0; k < (2 * n - 1) - (2 * i); k++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+		for (int i = n - 1; i > 0; i--) {
+			for (int j = 0; j < i - 1; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 0; k < (2 * n + 1) - (2 * i); k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
 		br.close();
 	}
 

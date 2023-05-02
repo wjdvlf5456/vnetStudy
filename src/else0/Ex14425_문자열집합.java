fd89c8da9032cf771c416e28a3aa81c14d20f981
최정필 <wjdvlf1407@gmail.com>
2023-05-02 16:15:47 +0900

    문자열 집합 정답

---
+++ src/else0/Ex14425_문자열집합.java
@@ -0,0 +1,48 @@
+package else0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Ex14425_문자열집합 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		List<String> sList = new ArrayList<String>();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		
+		for (int i = 0; i < n; i++) {
+			
+			String s = br.readLine();
+			sList.add(s);
+			
+		}
+		
+		int count = 0;
+		
+		for (int i = 0; i < m; i++) {
+			String s = br.readLine();
+			if (sList.contains(s)) {
+				count++;
+			}
+			
+		}
+		
+		System.out.println(count);
+		
+		
+		br.close();
+		
+		
+	}
+
+}

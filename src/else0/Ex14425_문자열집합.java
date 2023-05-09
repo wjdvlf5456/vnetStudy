ccf727873d28e9f59605707bb0c127dfd530276c
최정필 <wjdvlf1407@gmail.com>
2023-05-09 20:41:25 +0900

    11

--- src/else0/Ex14425_문자열집합.java
+++ src/else0/Ex14425_문자열집합.java
@@ -1,8 +1,10 @@
 package else0;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -14,15 +16,16 @@ public class Ex14425_문자열집합 {
 		List<String> sList = new ArrayList<String>();
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		
+		String s;
 		for (int i = 0; i < n; i++) {
 			
-			String s = br.readLine();
+			s = br.readLine();
 			sList.add(s);
 			
 		}
@@ -30,16 +33,18 @@ public class Ex14425_문자열집합 {
 		int count = 0;
 		
 		for (int i = 0; i < m; i++) {
-			String s = br.readLine();
+			s = br.readLine();
 			if (sList.contains(s)) {
 				count++;
 			}
 			
+			
 		}
 		
-		System.out.println(count);
+		bw.write(count+"");
 		
-		
+		bw.flush();
+		bw.close();
 		br.close();
 		
 		

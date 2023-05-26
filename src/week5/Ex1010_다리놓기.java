2dadb938825ead34c64596e174b366c1754f1c94
최정필 <wjdvlf1407@gmail.com>
2023-05-26 22:15:53 +0900

    1111

---
+++ src/week5/Ex1010_다리놓기.java
@@ -0,0 +1,32 @@
+package week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex1010_다리놓기 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int t = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			
+			
+		}
+		
+		
+		
+		
+		br.close();
+		
+	}
+
+}
